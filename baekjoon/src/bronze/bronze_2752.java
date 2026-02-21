a56e6a1210779ba4d13b57178d90e000b3ebb2a0
jinjin0528 <jin12lee@naver.com>
2026-02-21 16:39:17 +0900

    [Baekjoon] 2752  memory 15880KB time 112ms

---
+++ baekjoon/src/bronze/bronze_2752.java
@@ -0,0 +1,23 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bronze_2752 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = new int [3];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);   // 오름차순
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
