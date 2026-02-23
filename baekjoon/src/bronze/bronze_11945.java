bbf4d51b266660373cac15e596912ac3c8f88f08
jinjin0528 <jin12lee@naver.com>
2026-02-23 14:10:05 +0900

    [Baekjoon] 11945  memory 14080KB time 100ms

---
+++ baekjoon/src/bronze/bronze_11945.java
@@ -0,0 +1,20 @@
+package bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class bronze_11945 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < N; i++){
+            String bb = br.readLine();
+            bw.write(new StringBuilder(bb).reverse().toString());
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
