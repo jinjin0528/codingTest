423d38da289d4dfd141698f410c479964cbad0be
jinjin0528 <jin12lee@naver.com>
2026-02-26 13:26:45 +0900

    [Baekjoon] 17388  memory 14212KB time 104ms

---
+++ baekjoon/src/bronze/bronze_17388.java
@@ -0,0 +1,32 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bronze_17388 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int S = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int sum = S+K+H;
+
+        if(sum>=100){
+            System.out.println("OK");
+        }else{
+            if(sum<100){
+                int min = Math.min(S,Math.min(K,H));
+                if(min == S){
+                    System.out.println("Soongsil");
+                }else if(min == K){
+                    System.out.println("Korea");
+                }else{
+                    System.out.println("Hanyang");
+                }
+            }
+        }
+    }
+}
