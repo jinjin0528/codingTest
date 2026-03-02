d1406eb1cb4b0ff079d41b2e2ebd79d27b9c7cdb
jinjin0528 <jin12lee@naver.com>
2026-03-02 15:56:47 +0900

    [Baekjoon] 10818  memory 88176KB time 448ms

--- baekjoon/src/bronze/bronze_10818.java
+++ baekjoon/src/bronze/bronze_10818.java
@@ -3,24 +3,22 @@ package bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class bronze_2884 {
+public class bronze_10818 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int H = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        if(M<45){
-            M += 60-45;
-            if(H==0){
-                H = 23;
-            }else{
-                H -= 1;
-            }
-        }else{
-            M= M-45;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<N;i++){
+            int x = Integer.parseInt(st.nextToken());
+            if (x < min) min = x;
+            if (x > max) max = x;
         }
-        System.out.println(H+" "+M);
+        System.out.print(min+" "+max);
     }
 }
