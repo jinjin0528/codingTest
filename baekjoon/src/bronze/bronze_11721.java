c3ff52976e2ec0e4d6baa01b03a91400bf1e6929
jinjin0528 <jin12lee@naver.com>
2026-03-08 22:18:08 +0900

    [Baekjoon] 11721  memory 14240KB time 108s

---
+++ baekjoon/src/bronze/bronze_11721.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bronze_11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        for(int i=0; i<str.length(); i++) { // length길이 만큼 반복
+            System.out.print(str.charAt(i)); // char로 하나하나 프린트
+            if (i % 10 == 9) { // 만약 인덱스 번호가 9가 되면 줄바꿈
+                System.out.println();
+            }
+        }
+    }
+}
