d680766e81b4de081f9aa7802eb551c500b14f13
jinjin0528 <jin12lee@naver.com>
2026-03-12 14:11:55 +0900

    [Baekjoon] 2446  memory 20888KB time 360s

---
+++ baekjoon/src/bronze/bronze_2446.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bronze_2446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*N)-(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=N-2;i>=0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2 * N) - (2 * i) - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
