bddf2724956ccce73a00e32688cdfeaca258706a
jinjin0528 <jin12lee@naver.com>
2026-03-14 17:15:05 +0900

    [Baekjoon] 10810  memory 15892KB time 124s

--- baekjoon/src/bronze/bronze_10810.java
+++ baekjoon/src/bronze/bronze_10810.java
@@ -8,14 +8,28 @@ import java.util.StringTokenizer;
 public class bronze_10810 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =  new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        for(int n=0;n<M;n++){
-            st =  new StringTokenizer(br.readLine()," ");
+
+        int[] arr = new int[N + 1];
+
+        for(int n = 0; n < M; n++){
+
+            st = new StringTokenizer(br.readLine());
+
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
+
+            for(int l = i; l <= j; l++){
+                arr[l] = k;
+            }
+        }
+
+        for(int x = 1; x <= N; x++){
+            System.out.print(arr[x] + " ");
         }
     }
 }
