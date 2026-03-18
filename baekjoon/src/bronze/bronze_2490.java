9c3203aa5068377a416ea6f4bd7cba7d6bc44375
jinjin0528 <jin12lee@naver.com>
2026-03-18 16:06:36 +0900

    [Baekjoon] 2490  memory 14152KB time 100s

--- baekjoon/src/bronze/bronze_2490.java
+++ baekjoon/src/bronze/bronze_2490.java
@@ -9,16 +9,31 @@ public class bronze_2490 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 3; i++) {
-            int count = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int count = 0;
             for(int j = 0; j < 4; j++) {
-                count += Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                if(num == 0){
+                    count++;
+                }
             }
-            if(count == 3) System.out.println("A");
-            else if(count == 2) System.out.println("B");
-            else if(count == 1) System.out.println("C");
-            else if(count == 0) System.out.println("E");
-            else if(count == 4) System.out.println("D");
+            switch (count){
+                case 1:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("C");
+                    break;
+                case 4:
+                    System.out.println("D");
+                    break;
+                case 0:
+                    System.out.println("E");
+                    break;
+            }
         }
     }
 }
