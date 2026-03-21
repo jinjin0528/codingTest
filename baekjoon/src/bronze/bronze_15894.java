145f345fb0b2465f63e117ad44af1d6d818a0020
jinjin0528 <jin12lee@naver.com>
2026-03-21 17:32:13 +0900

    [Baekjoon] 15894  memory 14228KB time 100s

---
+++ baekjoon/src/bronze/bronze_15894.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bronze_15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println(N*4);
+    }
+}
