24403f84dc14890e7c90b52f51a277abe70da434
jinjin0528 <jin12lee@naver.com>
2026-03-22 14:54:58 +0900

    [Baekjoon] 10886  memory 14316KB time 100s

--- baekjoon/src/bronze/bronze_10886.java
+++ baekjoon/src/bronze/bronze_10886.java
@@ -8,17 +8,17 @@ public class bronze_10886 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int not_cute = 0;
+       // int not_cute = 0;
         int cute = 0;
         for(int i=0;i<N;i++){
             int c = Integer.parseInt(br.readLine());
             if(c==1){
                 cute+=1;
-            }else {
+            }/*else {
                 not_cute+=1;
-            }
+            }*/
         }
-        if(not_cute>cute){
+        if(cute<N/2){
             System.out.println("Junhee is not cute!");
         }else{
             System.out.println("Junhee is cute!");
