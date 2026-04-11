22e161f8ab8e43e14eefb6338082a777c65b6db7
jinjin0528 <jin12lee@naver.com>
2026-04-11 14:51:01 +0900

    [Baekjoon] 2863  memory 14108KB time 104ms

---
+++ baekjoon/src/bronze/bronze_2863.java
@@ -0,0 +1,35 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bronze_2863 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double[] arr = new double[4];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        st = new StringTokenizer(br.readLine()," ");
+        double C = Double.parseDouble(st.nextToken());
+        double D = Double.parseDouble(st.nextToken());
+
+        arr[0] = (A/C) + (B/D);
+        arr[1] = (C/D) + (A/B);
+        arr[2] = (D/B) + (C/A);
+        arr[3] = (B/A) + (D/C);
+
+        double MAX = Double.MIN_VALUE;
+        int n = 0;
+        for(int i=0;i<4;i++){
+            if(MAX<arr[i]){
+                MAX=arr[i];
+                n = i;
+            }
+        }
+        System.out.println(n);
+    }
+}
