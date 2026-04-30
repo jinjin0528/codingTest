1d57e74b7e1b1e5d394d502d44e60fd1993dcaef
jinjin0528 <jin12lee@naver.com>
2026-04-30 23:58:32 +0900

    [Programmers] 문자열로 변환 0.02ms, 87.5MB

---
+++ programmers/src/codingTest/code13.java
@@ -0,0 +1,16 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class code13 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String answer = Integer.toString(n);
+        System.out.println(answer);
+
+
+    }
+}
