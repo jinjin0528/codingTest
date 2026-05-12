4ea42557987503cdba17e896cf951ff24172ec01
jinjin0528 <jin12lee@naver.com>
2026-05-12 12:16:00 +0900

    [Programmers] 뒤에서 5등 위로  0.21ms, 61.5MB

---
+++ programmers/src/codingTest/code22.java
@@ -0,0 +1,25 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class code22 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = input.length;
+        int[] num_list = new int[n];
+        for(int i=0;i<n;i++){
+            num_list[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(num_list);
+        int m = 5;
+        int[] answer = new int[num_list.length - m];
+        for(int i = m;i<num_list.length;i++){
+            answer[i-m] = num_list[i];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
