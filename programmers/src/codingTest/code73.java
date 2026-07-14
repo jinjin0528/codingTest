1b6add2bef9c9fe202890a7c41a20daffde0b403
jinjin0528 <jin12lee@naver.com>
2026-07-14 20:58:39 +0900

    [Programmers] l로 만들기     8.51ms, 91.4MB

---
+++ programmers/src/codingTest/code73.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class code73 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.replaceAll("[a-k]","l"));
+    }
+}
