21b730537a67845cb98952c94ca5e9f298947f3a
jinjin0528 <jin12lee@naver.com>
2026-08-02 22:56:13 +0900

    [Programmers] 문자열이 몇 번 등장하는지 세기 6.10ms, 118MB

--- programmers/src/codingTest/code83.java
+++ programmers/src/codingTest/code83.java
@@ -7,14 +7,14 @@ import java.io.InputStreamReader;
 public class code83 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        public int solution(String myString, String pat) {
-            int answer = 0;
+        String myString = br.readLine();
+        String pat = br.readLine();
+        int answer = 0;
             for (int i = 0; i <= myString.length() - pat.length(); i++) {
                 if (myString.substring(i).startsWith(pat)) {
                     answer++;
                 }
             }
-            return answer;
+        System.out.println(answer);
     }
 }
