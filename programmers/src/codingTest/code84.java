5af5f8a6bf3ee084660bd9ec7ae2a9d45aee7b23
jinjin0528 <jin12lee@naver.com>
2026-08-18 21:33:19 +0900

    [Programmers] 특별한 이차원 배열 2 0.00ms, 5.01MB

--- programmers/src/codingTest/code84.java
+++ programmers/src/codingTest/code84.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class code84 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine(" "));
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
